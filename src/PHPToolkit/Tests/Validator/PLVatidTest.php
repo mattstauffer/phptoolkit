af3f2b7a2e09cc09bcaf9128dd3a054d9fe97d7e
tomaszsobczak <tomeksobczak@gmail.com>
2012-09-08 00:07:54 +0200

    Introduced Validator\PLVatid

---
+++ src/PHPToolkit/Tests/Validator/PLVatidTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace PHPToolkit\Validator;
+
+require_once __DIR__ . '/../../Validator/PLVatid.php';
+
+class PLVatidTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider providerPLVatid
+     */
+    public function testIsValid($expected, $vatid, $message)
+    {
+        $validator = new PLVatid($vatid);
+
+        $this->assertEquals($expected, $validator->isValid(), $message);
+    }
+
+    public function providerPLVatid()
+    {
+        return array(
+            array(false, '123465789',       'Too short'),
+            array(false, '12345678901',     'Too long'),
+            array(false, '1234567890',      'Invalid control sum'),
+            array(false, 'ABC1234567',      'Too short due to letters'),
+            array(false, '5250008199',      'Invalid VATID'),                        // Narodowy Bank Polski with changed control sum (8 -> 9)
+            array(true,  '5250008198',      'Valid VATID'),                          // Narodowy Bank Polski
+            array(true,  '525-00-08-198',   'Valid VATID with dashes'),              // Narodowy Bank Polski
+            array(true,  'PL5250008198',    'Valid VATID with EU prefix'),           // Narodowy Bank Polski
+            array(true,  'PL525-00-08-198', 'Valid VATID with EU prefix and dashes') // Narodowy Bank Polski
+        );
+    }
+}
---
+++ src/PHPToolkit/Validator/PLVatid.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace PHPToolkit\Validator;
+
+class PLVatid
+{
+    const MODULO_DIVISOR     = 11;
+    const VATID_VALID_LENGTH = 10;
+
+    private $vatid = null;
+
+    public function __construct($vatid)
+    {
+        $this->vatid = $vatid;
+    }
+
+    public function isValid($vatid = null)
+    {
+        if (is_null($vatid)) {
+            $vatid = $this->vatid;
+        }
+
+        $digits = array_filter(preg_split('//', $vatid, -1, PREG_SPLIT_NO_EMPTY), 'is_numeric');
+        $length = count($digits);
+        $sum    = 0;
+
+        if (self::VATID_VALID_LENGTH != $length) {
+            return false;
+        }
+
+        foreach ($this->weights() as $weight) {
+            $sum += $weight * array_shift($digits);
+        }
+
+        return (($sum % self::MODULO_DIVISOR) == array_shift($digits));
+    }    
+
+    private function weights()
+    {
+        return array(6, 5, 7, 2, 3, 4, 5, 6, 7);
+    }
+}
